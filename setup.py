932561d26310ed39647e730348ceba1ee28849f0
Soude90 <59317883+Soude90@users.noreply.github.com>
2021-07-10 21:16:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setuptools.setup(
 	version = '0.0.1',
 	author = 'Soumyadeep Barman',
 	author_email = 'soumronaldo09@gmail.com',
-    description = 'This is a Text preprocessing package',
-    long_description =  long_description,
-    long_description_content_type = 'text/markdown',
-    packages = setuptools.find_packages(),
-    classifiers = [
-    'Programming Language:: Python :: 3']
+        description = 'This is a Text preprocessing package',
+        long_description =  long_description,
+        long_description_content_type = 'text/markdown',
+        packages = setuptools.find_packages(),
+        classifiers = [
+        'Programming Language:: Python :: 3']
 	)
